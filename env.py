1806704c3744aaea484452fb83dcc41c185ce958
christiangoran <christian.goran@protonmail.com>
2023-10-11 13:31:50 +0200

    Initial commit

---
+++ env.py
@@ -0,0 +1,2 @@
+import os
+os.environ['CLOUDINARY_URL'] = 'cloudinary://874945589933534:Fa0UWCX4pQsjxIIMvMyMR03gj_w@dzw4z92rn'
---
+++ userprofile/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_y7ymw3.jpg'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.user.username}' profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(create_profile, sender=User)
