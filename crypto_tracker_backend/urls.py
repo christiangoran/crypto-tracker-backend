4dcb93f552fe47d006431f92f70934f654539491
christiangoran <christian.goran@protonmail.com>
2023-10-12 21:32:36 +0200

    various files from creating apps

--- crypto_tracker_backend/urls.py
+++ crypto_tracker_backend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('userprofile.urls')),
     path('', include('currencypost.urls')),
+    path('', include('currency.urls')),
 ]
