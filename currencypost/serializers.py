fd561ae2bde63de4f122c66d55abf5a51777e1c1
christiangoran <christian.goran@protonmail.com>
2023-10-12 08:34:27 +0200

    added image validation in serializer

--- currencypost/serializers.py
+++ currencypost/serializers.py
@@ -5,6 +5,27 @@ from .models import CurrencyPost
 class CurrencyPostSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
     is_owner = serializers.SerializerMethodField()
+    profile_image = serializers.ImageField(
+        source='user.userprofile.image', read_only=True
+    )
+    profile_id = serializers.IntegerField(
+        source='user.userprofile.id', read_only=True
+    )
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
 
     def get_is_owner(self, obj):
         return obj.user == self.context['request'].user
