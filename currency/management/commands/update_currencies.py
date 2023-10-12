758cb863ce8cf13987dece92029da056bfed229e
christiangoran <christian.goran@protonmail.com>
2023-10-12 21:31:26 +0200

    added custom django admin command to test and update crypto data in database

---
+++ currency/management/commands/update_currencies.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from currency.models import Currency
+
+
+class Command(BaseCommand):
+    """
+    To update currencies data from CoinMarketCap, run:
+    """
+    help = 'Update currencies data from CoinMarketCap'
+
+    def handle(self, *args, **kwargs):
+        Currency.update_currency_data()
+        self.stdout.write(self.style.SUCCESS(
+            'Successfully updated currencies data'))
