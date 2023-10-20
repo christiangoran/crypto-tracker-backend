2f71590ed373405a2bfc7bebedd6ec8a4fc5a882
christiangoran <christian.goran@protonmail.com>
2023-10-20 12:23:14 +0200

    added JSON web tokens and configured them

--- crypto_tracker_backend/urls.py
+++ crypto_tracker_backend/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls'))
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('userprofile.urls')),
     path('', include('currencypost.urls')),
     path('', include('currency.urls')),
