2c874bd1c2ac820260b7d694b416694990bc9e88
christiangoran <christian.goran@protonmail.com>
2023-10-24 10:48:37 +0200

    CSRF and SESSION cookies in settings.py

--- crypto_tracker_backend/settings.py
+++ crypto_tracker_backend/settings.py
@@ -54,6 +54,11 @@ REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'crypto_tracker_backend.serializers.CurrentUserSerializer',
 }
 
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
