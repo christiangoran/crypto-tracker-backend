bd0888ac535f988ef00332a876ab7d80c0e038c0
christiangoran <christian.goran@protonmail.com>
2023-11-16 10:41:16 +0100

    Add test for successful Currency model creation

--- currency/tests.py
+++ currency/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from currency.models import Currency
 
-# Create your tests here.
+
+class CurrencyModelTest(TestCase):
+    def setUp(self):
+        Currency.objects.create(
+            currency_id='1', name='Bitcoin', symbol='BTC',
+            current_price=40000, market_cap=800000000, total_volume=50000
+        )
+
+    def test_currency_creation(self):
+        bitcoin = Currency.objects.get(name='Bitcoin')
+        self.assertEqual(bitcoin.symbol, 'BTC')
+        self.assertEqual(bitcoin.current_price, 40000)
