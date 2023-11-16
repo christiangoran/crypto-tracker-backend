1e962358fb9308a597309b03f795dc7c4b43a057
christiangoran <christian.goran@protonmail.com>
2023-11-16 09:51:03 +0100

    Add GET test for IsOwnerOrReadOnly permission

--- crypto_tracker_backend/tests.py
+++ crypto_tracker_backend/tests.py
@@ -1,8 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.contrib.auth import get_user_model
 from .serializers import CurrentUserSerializer
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, force_authenticate
 from rest_framework import status
+from .permissions import IsOwnerOrReadOnly
 
 
 class CurrentUserSerializerTest(TestCase):
@@ -30,3 +31,26 @@ class LogoutRouteTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertNotIn('jwt_auth_cookie', response.cookies)
         self.assertNotIn('jwt_auth_refresh_cookie', response.cookies)
+
+
+class RootRouteTest(APITestCase):
+    def test_root_route_returns_message(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {"message": "Crypto Tracker API"})
+
+
+class IsOwnerOrReadOnlyTest(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='Testyroonie', password='password123')
+        self.other_user = get_user_model().objects.create_user(
+            username='hacker', password='password123')
+        self.permission = IsOwnerOrReadOnly()
+        self.factory = RequestFactory()
+
+    def test_safe_method_by_any_user(self):
+        request = self.factory.get('/')
+        request.user = self.other_user
+        self.assertTrue(self.permission.has_object_permission(
+            request, None, obj=self.user))
