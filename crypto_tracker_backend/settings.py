1b4253642f30db473a3cab5f61385c3f5276314d
christiangoran <christian.goran@protonmail.com>
2023-11-16 11:30:40 +0100

    Add test for CurrencyPostList view response and query handling

--- crypto_tracker_backend/settings.py
+++ crypto_tracker_backend/settings.py
@@ -201,3 +201,18 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
--- currencypost/tests.py
+++ currencypost/tests.py
@@ -1,3 +1,5 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
 from django.test import TestCase
 from django.contrib.auth.models import User
 from currency.models import Currency
@@ -18,3 +20,21 @@ class CurrencyPostModelTest(TestCase):
         self.assertEqual(post.user.username, 'testie')
         self.assertEqual(post.content, 'Exciting times for Bitcoin!')
         self.assertEqual(post.currency.name, 'Bitcoin')
+
+
+class CurrencyPostListViewTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testie', password='password123')
+        self.currency = Currency.objects.create(
+            currency_id='1', name='Bitcoin', symbol='BTC', current_price=40000, market_cap=800000000, total_volume=50000)
+        CurrencyPost.objects.create(user=self.user, topic='Bitcoin Rising',
+                                    content='Exciting times for Bitcoin!', currency=self.currency)
+
+    def test_get_currency_posts(self):
+        actual_count = CurrencyPost.objects.count()
+        response = self.client.get('/currencyposts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), actual_count)
+        self.assertEqual(response.data['results']
+                         [0]['topic'], 'Bitcoin Rising')
