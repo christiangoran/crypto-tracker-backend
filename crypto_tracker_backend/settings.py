e2be95a63e19769c18922d565db21c76fb41a8ec
christiangoran <christian.goran@protonmail.com>
2024-07-15 16:43:58 +0200

    Update settings.py

--- crypto_tracker_backend/settings.py
+++ crypto_tracker_backend/settings.py
@@ -50,17 +50,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'localhost',
     'localhost:3000',
     '127.0.0.1',
     '127.0.0.1:3000',
     'localhost:5173',
     '127.0.0.1:8000',
-    'http://192.0.2.1:3000',  # Adjust this IP to the correct one
-    '8000-christiango-cryptotrack-mdqxlbt6rmt.ws-eu105.gitpod.io',
-    '3001-christiango-cryptotrack-p0j28pes4st.ws-eu106.gitpod.io',
-    '3000-christiango-cryptotrack-bobhep5qfip.ws-eu105.gitpod.io',
-    '3000-christiango-cryptotrack-jhy91ueo5b0.ws-eu106.gitpod.io',
+    'http://192.168.178.20:3000',
     'crypto-tracker-heroku-b5acda38c706.herokuapp.com',
     'crypto-tracker-frontend-heroku-96a37577175c.herokuapp.com',
     'christiangoran.github.io',
@@ -97,18 +92,6 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-# MIDDLEWARE = [
-#     'corsheaders.middleware.CorsMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'allauth.account.middleware.AccountMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-# ]
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,12 +101,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://christiangoran.github.io",
-    "https://crypto-tracker-frontend-heroku-96a37577175c.herokuapp.com",
-    "http://localhost:3000",
+    'https://christiangoran.github.io',
+    'https://crypto-tracker-frontend-heroku-96a37577175c.herokuapp.com',
+    'http://localhost:3000',
+    os.environ.get('CLIENT_ORIGIN'),
+    os.environ.get('CLIENT_ORIGIN_DEV'),
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -148,9 +134,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'crypto_tracker_backend.wsgi.application'
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 if 'DEV' in os.environ:
     DATABASES = {
         'default': {
@@ -160,9 +143,7 @@ if 'DEV' in os.environ:
     }
 else:
     DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get("DATABASE_URL")
-        )
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 AUTH_PASSWORD_VALIDATORS = [
