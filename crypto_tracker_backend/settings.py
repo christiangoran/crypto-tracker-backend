67cf7ef9ef86bcde48d799b8ebd77c945174e6d3
christiangoran <christian.goran@protonmail.com>
2024-07-15 15:27:02 +0200

    Remove incorrect allath middleware

--- crypto_tracker_backend/settings.py
+++ crypto_tracker_backend/settings.py
@@ -106,7 +106,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    # 'allauth.account.middleware.AccountMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
