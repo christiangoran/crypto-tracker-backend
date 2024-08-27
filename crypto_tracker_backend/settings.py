a40a2e633cc73a49846e560403c23cde403d842c
christiangoran <christian.goran@protonmail.com>
2024-08-27 10:45:04 +0200

    update settings.py

--- crypto_tracker_backend/settings.py
+++ crypto_tracker_backend/settings.py
@@ -32,6 +32,7 @@ if 'DEV' not in os.environ:
     ]
 
 REST_USE_JWT = True
+JWT_AUTH_HTTPONLY = False
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-app-refresh'
